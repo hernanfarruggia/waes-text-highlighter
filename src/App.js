85a56b44257483b931dda45c56a659439c43c422
Hernan Faarruggia <hernan.farruggia@gmail.com>
2019-07-17 00:32:03 -0300

    - Setting up initial react structure - Initial layout and styling - Removed unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      
+      <div className="block">
+        <textarea className="text-area" rows="10" placeholder="Insert your text here and click 'Add Text'!" />
+        <button className="btn">Add Text</button>
+      </div>
+      
+      <div className="block">
+        <div className="colors">
+          <button className="btn red">Red</button>
+          <button className="btn yellow">Yellow</button>
+          <button className="btn green">Green</button>
+        </div>
+        <div className="text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+      </div>
+      
+      <div className="block">
+        <div className="colors">
+          <button className="btn red">Red</button>
+          <button className="btn yellow">Yellow</button>
+          <button className="btn green">Green</button>
+        </div>
+        <div className="text">
+          Plese select a color to list selected text!
+        </div>
+      </div>
     </div>
   );
 }
